c98a7c4ec82314dd9b4d3a86637fd4656bb184cf
Travis Eubanks <TRAVIS@traviss-air.attlocal.net>
2015-03-01 17:26:19 -0600

    initial commit+ generated posts_controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,35 @@
+class PostsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+    def params_hash
+      # params.require(:post).permit(:title, :body)
+    end
+end
