b11ab86c13f0d7dd0b7f4c8254a7e35a92d8a5de
Travis Eubanks <TRAVIS@Traviss-MacBook-Air.local>
2015-03-03 11:21:46 -0600

    add edit and destroy btns on posts#show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,11 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
 
+    if @post.destroy
+      redirect_to posts_url
+    end
   end
 
   private
