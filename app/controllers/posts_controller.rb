9db55420f2aa403a9ebd20f73052d7b78c72b8ee
Travis Eubanks <TRAVIS@Traviss-MacBook-Air.local>
2015-03-03 11:04:12 -0600

    adds edit and update actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end
 
   def create
@@ -26,7 +26,13 @@ class PostsController < ApplicationController
     end
 
   def update
+    @post = Post.find(params[:id])
 
+    if @post.update(params[:post].permit(:title, :body))
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   def destroy
