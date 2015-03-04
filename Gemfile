346938bdc39fcbb525b4227b5e67798778afa23d
Travis Eubanks <TRAVIS@Traviss-MacBook-Air.local>
2015-03-04 11:43:39 -0600

    Revert 9f724fe..e6df075
    This rolls back to commit 9f724fe5d8c9c2badb91c6d0f894fd08c7bce936.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,5 @@
 source 'https://rubygems.org'
 
-
-gem "paperclip"
 gem 'bootstrap-sass'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,7 @@
 class CommentsController < ApplicationController
 end
 
-
-# find the post who matches the post_id
-# take that post, create a new row in the comments with matching ID's and only allow input for cols name and body
 def create
   @post = Post.find(param[:post_id])
   @comment = @post.comments.create(params[:comment].permit(:name, :body))
-
-  redirect_to post_path(@post)
 end
