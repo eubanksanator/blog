088abf8f674a0c1e0347f0f7d9712309dd975a72
Travis Eubanks <TRAVIS@Traviss-MacBook-Air.local>
2015-03-11 11:14:47 -0500

    styles panel background and typography

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,13 @@
 class CommentsController < ApplicationController
+
+def new
+  @post = Post.find(param[:post_id])
+  @comment = @post.comments.create(params[:comment].permit(:name, :body))
 end
 
 def create
   @post = Post.find(param[:post_id])
   @comment = @post.comments.create(params[:comment].permit(:name, :body))
+end
+
 end
